9050d8590d304866a4255ed9830eebdf99d79153
NickelKnock <156145154+NickelKnock@users.noreply.github.com>
2024-03-10 16:48:42 -0500

    Pushed the wrong one
    oops

--- HW6_1_2_OOP.py
+++ HW6_1_2_OOP.py
@@ -6,16 +6,10 @@ class ResistorNetwork2(ResistorNetwork):
     def __init__(self):
         super().__init__()
 
-    def GetElementDeltaV(self, elementName):
-        element = self.GetElementByName(elementName)
-        if isinstance(element, Resistor):
-            return element.GetVoltageDrop()
-        elif isinstance(element, VoltageSource):
-            return element.Value
-        else:
-            return 0  # Return 0 for unknown elements
+        self.Loops = []  # initialize an empty list of loop objects in the network
+        self.Resistors = []  # initialize an empty a list of resistor objects in the network
+        self.VSources = []  # initialize an empty a list of source objects in the network
 
-    # Rest of the ResistorNetwork2 class code...
 
     def AnalyzeCircuit(self):
         """
@@ -25,12 +19,14 @@ class ResistorNetwork2(ResistorNetwork):
         :return: a list of the currents in the resistor network
         """
         # need to set the currents to that Kirchoff's laws are satisfied
-        i0 = [0.1, 0.1, 0.1]  # define an initial guess for the currents in the circuit
+        i0 = [0.1, 0.1, 0.1, 0.1, 0.1]  # define an initial guess for the currents in the circuit
         i = fsolve(self.GetKirchoffVals, i0)
         # print output to the screen
         print("I1 = {:0.1f}".format(i[0]))
         print("I2 = {:0.1f}".format(i[1]))
         print("I3 = {:0.1f}".format(i[2]))
+        print("I4 = {:0.1f}".format(i[3]))
+        print("I5 = {:0.1f}".format(i[4]))
         return i
 
     def GetKirchoffVals(self, i):
@@ -41,17 +37,24 @@ class ResistorNetwork2(ResistorNetwork):
         :param i: a list of currents relevant to the circuit
         :return: a list of loop voltage drops and node currents
         """
-        # set current in resistors in the top loop.
+        # set current in resistors in the top loop
         self.GetResistorByName('ad').Current = i[0]  # I_1 in diagram
-        self.GetResistorByName('bc').Current = i[0]  # I_1 in diagram
+        self.GetResistorByName('bc').Current = i[1]  # I_2 in diagram
         self.GetResistorByName('cd').Current = i[2]  # I_3 in diagram
-        # set current in resistor in bottom loop.
-        self.GetResistorByName('ce').Current = i[1]  # I_2 in diagram
+
+        # set current in resistors in the bottom loop
+        self.GetResistorByName('ce').Current = i[3]  # I_4 in diagram
+        self.GetResistorByName('de').Current = i[4]  # I_5 in diagram
+
         # calculate net current into node c
-        Node_c_Current = sum([i[0], i[1], -i[2]])
+        Node_c_Current = sum([i[1], -i[2], -i[3]])
+
+        # calculate net current into node d
+        Node_d_Current = sum([i[0], i[2], -i[4]])
 
         KVL = self.GetLoopVoltageDrops()  # two equations here
         KVL.append(Node_c_Current)  # one equation here
+        KVL.append(Node_d_Current)  # one equation here
         return KVL
 
 def main():
